c708beba1b8f2061e32ab1ef36b55ad989f8b8bd
sushil-cmd-r <kandharesushil10@gmail.com>
2024-09-15 19:46:06 +0530

    Add bool object

--- object/bool.go
+++ object/bool.go
@@ -1,7 +1,5 @@
 package object
 
-import "fmt"
-
 type Bool struct {
 	value bool
 }
@@ -12,11 +10,4 @@ func (b *Bool) Type() ObjType {
 
 func NewBool(val bool) *Bool {
 	return &Bool{value: val}
-}
-
-func AsBool(obj Obj) (bool, error) {
-	if obj.Type() != BoolObj {
-		return false, fmt.Errorf("invalid cast: casting %s to bool", obj.Type())
-	}
-	return obj.(*Bool).value, nil
 }
